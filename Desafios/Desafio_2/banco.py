4e0d34ff5c650dbf042e17587efc17b9ac5d46e0
C2002G <claudiogabrieldev@gmail.com>
2025-08-18 19:35:53 -0300

    preparativos para o desafio 2

---
+++ Desafios/Desafio_2/banco.py
@@ -0,0 +1,77 @@
+"""
+fazer um sistema bancario com as seguintes funções
+    trabalha apenas com um usuario
+    todos os depositos sao armazenados em uma variavel e exibidos em extrato
+    permite realizar 3 saques diarios
+    com limite de 500R$ por saque
+    caso n ter sado aparecer
+    saques armazenados em um variavel para mostra me extrato
+    NO FIM DEVE LISTRAR TODOS Os depositos e saques realizado na conta
+    deve utulizar usando formato R$ xxx.xx
+"""
+
+import textwrap
+
+def menu():    
+    menu = """
+    [d] depositar
+    [s] sacar
+    [e] extrato
+    [q] sair
+    ==> """
+    return input(textwrap.dedent(menu))
+
+def main():
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numer_saques = 0
+    LIMITE_SAQUES = 3
+
+    while True:
+
+        opcao = menu()
+
+        if opcao == "d":
+            valor = float(input("Informe o valor do deposito: "))
+
+            saldo, extrato = depos
+            if valor > 0:
+                saldo += valor
+                extrato += f"deposito: R$ {valor:.2f}\n"
+
+            else:
+                print("operação falhou")
+
+        elif opcao == "s":
+            valor = float(input("informe o valor do sque: "))
+
+            excedeu_saldo = valor > saldo
+            excedeu_limite = valor > limite
+            excedeu_saques = numer_saques >= LIMITE_SAQUES
+
+            if excedeu_saldo:
+                print("vc n tem saldo suficiente")
+            elif excedeu_limite:
+                print("valor do saque excede o limite")
+            elif excedeu_saques:
+                print("excedeu o limite de saque diario")
+
+            elif valor > 0:
+                saldo -= valor
+                extrato += f"saque: R$ {valor:.2f}\n "
+
+            else:
+                print('operação falhou')
+
+        elif opcao == "e":
+            print("\n================== EXTRATO ================")
+            print("não foram realizadas movimentações" if not extrato else extrato)
+            print(f"\nSaldo: R$ {valor:.2f} ")
+            print("\n================== EXTRATO ================")
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("operação invalida")
--- trilha-python-dio-main/01 - Estrutura de dados/01 - Listas/05_iterar_listas.py
+++ trilha-python-dio-main/01 - Estrutura de dados/01 - Listas/05_iterar_listas.py
@@ -4,5 +4,5 @@ for carro in carros:
     print(carro)
 
 
-for indice, carro in enumerate(carros):
-    print(f"{indice}: {carro}")
+for vaga, carro in enumerate(carros):
+    print(f"{vaga}: {carro}")
